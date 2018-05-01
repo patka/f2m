4737fba9c9d5c5bb196b3042790c1de32f82f0f6
Patrick Kranz <patrick@got-email.de>
2018-05-01 10:12:02 +0200

    Added OptParse in order to specify an output folder and basic metadata conversion

--- README.md
+++ README.md
@@ -1,13 +1,18 @@
 # f2m
 A small ruby script that converts flac files to mp3
 
-This script converts flac music files to mp3. If you provide a directory as the applications argument it will convert all files inside the directory to mp3. It will do this by creating the same folder structure inside the folder where the script was run. If this happens to be the place where the source foulder is then the mp3 will be put right next to the flac files.
+This script converts flac music files to mp3. If you provide a directory as the applications argument it will convert all files inside the directory to mp3. By default all files will be placed in a folder called 'converted' but you can change this by providing an output folder using the -o option.
 
-This script requires lame and flac to be installed and in the execution PATH.
+This script requires lame and flac to be installed and to be available in the execution PATH.
 
-It currently does not convert any kind of metadata nor is the preset for lame changeable. It uses the preset medium which should result in varibale bitrate files with roughly 192kbit.
+It currently only converts the basic metadata (artist, track number, track title and album title). It uses the preset medium which should result in varibale bitrate files with roughly 192kbit.
 
-The script fills the gap for me that I have my whole CD collection ripped to flac but most of my music devices, especially in the car, refuse to play this file format :(
+The script fills the gap for me that I have my whole CD collection ripped to flac but most of my music devices, especially in the car, refuse to play this file format (I am also looking at you Apple)
+
+You need to install two ruby gems for this:
+```
+$ gem install ruby-mp3info flacinfo-rb
+```
 
 Run the script by typing:
 ```
--- f2m.rb
+++ f2m.rb
@@ -1,16 +1,37 @@
-class F2M
-  def convert(path)
-    convert_directory path
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'mp3info'
+require 'flacinfo'
+
+class F2M 
+  def initialize(options)
+    @options = options
+  end
+
+  def convert()
+    ensure_directory @options[:output_folder]
+    convert_directory @options[:source_folder]
   end
 
   private
   def convert_file(flac_file, target_folder)
     mp3_file = target_file flac_file, target_folder
-    puts mp3_file
     system "#{flac_command(quote(flac_file))} | #{mp3_command(quote(mp3_file))}"
+    set_id3_tags(flac_file, mp3_file)
   end
 
-  def convert_directory(path, current_dir = '.')
+  def set_id3_tags(flac_file, mp3_file)
+    flac_info = FlacInfo.new(flac_file)
+    mp3_info = Mp3Info.open(mp3_file) do |mp3|
+      mp3.tag.artist = flac_info.tags['ARTIST']
+      mp3.tag.album = flac_info.tags['ALBUM']
+      mp3.tag.title = flac_info.tags['TITLE']
+      mp3.tag.tracknum = Integer(flac_info.tags['TRACKNUMBER'])
+    end
+  end
+
+  def convert_directory(path, current_dir = '')
     unless File.exist? path
       puts "Path #{path} does not exist."
       return
@@ -18,12 +39,12 @@ class F2M
 
     if File.directory?(path)
       directory = File.join(current_dir, File.basename(path))
-      ensure_directory directory
+      ensure_directory File.join(@options[:output_folder], directory)
       Dir.foreach(path) do |entry|
         convert_directory(File.join(path, entry), directory) if no_navigation_entry? entry
       end
     else
-      convert_file path, current_dir if File.extname(path).downcase == '.flac'
+      convert_file path, File.join(@options[:output_folder], current_dir) if File.extname(path).downcase == '.flac'
     end
   end
 
@@ -54,5 +75,19 @@ class F2M
   end
 end
 
-instance = F2M.new
-instance.convert(ARGV[0])
+ARGV << '-h' if ARGV.empty?
+
+options = {
+  :output_folder => "converted",
+}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: f2m.rb [options] <source folder>"
+  opts.on("-o", "--output [FOLDER]", String,  "Output folder. Default is 'converted'") do |o|
+    options[:output_folder] = o || 'converted'
+  end
+end.parse!
+
+options[:source_folder] = ARGV[0]
+instance = F2M.new(options)
+instance.convert()
